14442637d47f732c6d6b1b31e0a3fe07b69aa5c1
sithu <122719409+sithu-khant@users.noreply.github.com>
2024-11-25 11:43:30 +0630

    11.25.08

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
   * (11.25.05) - Styled tooltip component.
   * (11.25.06) - Changed color propetry names.
   * (11.25.07) - Committed for safety.
+  * (11.25.08) - Finished `CategorySidebar` toolbar.
 * Sun Nov 24, 2024
   * (11.24.01) - Created `landing-pages` folder.
 * Sat Nov 23, 2024
--- frontend/components/shared/CategorySidebar.tsx
+++ frontend/components/shared/CategorySidebar.tsx
@@ -5,10 +5,10 @@ const CategorySidebar = () => {
   return (
     <>
       <div className='w-[220px] border border-y-0 border-l-0 p-2'>
-        <div className='border rounded w-full p-1 flex items-center'>
-          <p className='justify-start text-sm font-bold text-slate-600'>Subscriptions</p>
+        <div className='w-full p-1 flex items-center justify-end'>
+          {/* <p className='font-bold text-slate-600'>Category</p> */}
 
-          <div className='flex items-center justify-end w-full'>
+          <div className='flex items-center border rounded'>
             <i className="pi pi-plus p-2 text-sm text-slate-500 cursor-pointer"></i>
             <i className="pi pi-ellipsis-v p-1 text-sm text-slate-600 cursor-pointer"></i>
           </div>
